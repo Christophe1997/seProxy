757b77d9840ca5afcc5e52fba4bb2587e4c2791f
Christophe1997 <sdl.office.1997@gmail.com>
2019-04-27 22:02:52 +0800

    add simple class diagram

--- src/main/java/chris/seProxy/security/cipher/SimpleCipher.java
+++ src/main/java/chris/seProxy/security/cipher/SimpleCipher.java
@@ -4,6 +4,7 @@ package chris.seProxy.security.cipher;
  * Simple cipher without encrypt with initial vector
  */
 public interface SimpleCipher extends KeyGenerable {
+
     byte[] encrypt(byte[] plaintext, byte[] key) throws Exception;
 
     byte[] decrypt(byte[] ciphertext, byte[] key) throws Exception;
